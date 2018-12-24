c0abc1babeb94b2db0c446d744ca981423dd2290
abdesselam <abdesselam.mihoubi@gmail.com>
2018-12-24 15:19:15 +0100

    gase

--- src/app/components/map/Map.js
+++ src/app/components/map/Map.js
@@ -1,12 +1,36 @@
 import L from 'leaflet'
  export class Map {
-    constructor(mapPlaceholder,mapOptions){
-         this.map = new L.Map(mapPlaceholder, mapOptions);
-        }
+         constructor(mapPlaceholder,mapOptions){
+                 this.map = new L.Map(mapPlaceholder, mapOptions);
+                 
+                 console.log('--------------hasLyer',this.map.hasLayer(mapOptions.layers[0]))
+                //   this.map.remove(mapOptions.layers[0])
+         console.log('--------------hasLyer----',this.map.hasLayer(mapOptions.layers[0]))
+         console.log('--------------hasLyer-----------',mapOptions.layers[0])
+        // this.map.addLayer(mapOptions.layers[0])
+        // mapOptions.layers[0].addTo(this.map)
+        
+
+        fetch('https://maptimeboston.github.io/leaflet-intro/rodents.geojson').then(function(data){
+                        console.log('------------5555----',datra)
+        })
+        
+               
+ 
+                }
         get getMap(){
                 return this.map;
                 
         }
+
+        //   getJsons() {
+        //         // eslint-disable-next-line no-bitwise
+        //         const url = 'https://maptimeboston.github.io/leaflet-intro/rodents.geojson';
+            
+        //         const request = await fetch(url);
+        //         const json = await request.json();
+        //         return this.parse({ data: json });
+        //       }
 }
 
 
---
+++ src/app/providers/index.js
@@ -0,0 +1,3 @@
+export { default as EsriProvider } from './esriProvider';
+export { default as OpenStreetMapProvider } from './openStreetMapProvider';
+export { default as Provider } from './provider';
